ec2b42d4c39cb2821a2771ebe6327781289ad272
MoeenMaruf <moin.u.maruf@gmail.com>
2024-04-27 23:39:57 -0500

    test for dummy values

--- nequip/long_range/HirshFeld.py
+++ nequip/long_range/HirshFeld.py
@@ -1,38 +1,79 @@
 import torch
+# from nequip.data import AtomicData, AtomicDataDict
+import numpy as np
 
-NSpecies = ..
-Nat = ...
+from nequip.data.AtomicData import neighbor_list_and_relative_vec
 
-J = [Nat] = set_as_learnable parameter #Hardness
-X = [Nat] = set_as_learnable parameter #Chi - Electronegativity
-Q = [Nat] = set_as_learnable parameter #predicted charges
-r = [Nat][Nat] #interatomic distances
+#dummy values to test the code
+NSpecies = 2
+Nat = 8
+pos = [[2.7320, 4.3236, 3.5625],
+        [0.7503, 1.2541, 3.7962],
+        [1.0590, 4.2208, 5.0442],
+        [2.9842, 5.0778, 5.0649],
+        [1.3311, 4.7296, 3.2238],
+        [4.5336, 4.5982, 1.7281],
+        [0.0900, 4.9429, 2.9770],
+        [1.3005, 2.9135, 4.8747]]
+X = [0.98, 0.98, 0.98, 0.98, 3.16, 3.16, 3.16, 3.16]
+Q = [1,1,1,1,-1,-1,-1,-1]
+J = [0.1,0.1,0.1,0.1,0.2,0.2,0.2,0.2]
+sigma = [2,2,2,2,1,1,1,1]
 
-gamma = build_gamma()
-sigma = [Nat] = set_as_learnable parameter
+r_max = 10
 
-def getNeighAtoms:
+# J = [Nat] = set_as_learnable parameter #Hardness
+# X = [Nat] = set_as_learnable parameter #Chi - predicted Electronegativity
+# Q = [Nat] = set_as_learnable parameter #Calculated charges
+# r = [Nat][Nat] #interatomic distances
+
+# sigma = [Nat] = set_as_learnable parameter
+
+def getNeighAtoms():
     pass
 
-def build_gamma:
-    gamma = [Nat][Nat]
+def build_gamma():
+    gamma = np.zeros((Nat, Nat))
     for i in range(Nat):
         for j in range(Nat):
-            gamma[i][j] = torch.sqrt(sigma[i]**2 + sigma[j]**2)
+            gamma[i][j] = np.sqrt(sigma[i]**2 + sigma[j]**2)
     return gamma        
 
-def build_r:
-    pass 
 
-def getELong:
-    Elong = 0
+def build_r(pos, r_max):
+    # Ensure pos is a numpy array
+    pos = np.array(pos)
+    
+    # Calculate interatomic distances
+    n_atoms = len(pos)
+    r = np.zeros((n_atoms, n_atoms))
+    for i in range(n_atoms):
+        for j in range(i+1, n_atoms):
+            distance = np.linalg.norm(pos[i] - pos[j])
+            if distance <= r_max:
+                r[i][j] = distance
+                r[j][i] = distance
+    
+    return r
+
+r = build_r(pos,r_max)
+
+def getELong():
+    Eelec = 0
     for i in range(Nat):
         add_part = X[i]*Q[i] + 1/2*J[i]*Q[i]**2
-        for j in range(Nneigh):
+        for j in range(i+1, Nat):
             rij = r[i][j]
             gammaij = gamma[i][j]
-            Eelec = torch.erf(rij/(sqrt(2)*gammaij))*Q[i]*Q[j]/rij
-        Eelec += Q[i]**2/(2*sigma[i]*sqrt(torch.pi))
+            Eelec = torch.erf(torch.tensor(rij/(np.sqrt(2)*gammaij)))*Q[i]*Q[j]/rij
+        Eelec += Q[i]**2/(2*sigma[i]*np.sqrt(torch.pi))
     
-    return Elong
-     
+    return Eelec
+    
+[edge_index, shifts, cell_tensor]= neighbor_list_and_relative_vec(pos,1.4)
+
+
+gamma = build_gamma()
+Elong = getELong()
+print(Elong)
+
